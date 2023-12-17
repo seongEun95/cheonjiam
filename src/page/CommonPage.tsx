d1f738e4f2b944ea04ebcef4d84625f858320c0f
seong eun <17adm@naver.com>
2023-12-17 10:59:35 +0900

    공통 페이지 추가

--- src/page/CommonPage.tsx
+++ src/page/CommonPage.tsx
@@ -1,13 +1,23 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react';
+
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import UserMenu from '../components/UserMenu';
 
 export default function CommonPage() {
 	return (
-		<>
+		<div css={commonWrapCss}>
 			<Header />
+			<UserMenu />
 			<Outlet />
 			<Footer />
-		</>
+		</div>
 	);
 }
+
+const commonWrapCss = css`
+	padding: 0 0 0 222px;
+`;
