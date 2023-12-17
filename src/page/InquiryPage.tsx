2ef0e5047ecbce5e189e9ad257ee5c01cb79a584
seong eun <17adm@naver.com>
2023-12-17 10:58:18 +0900

    페이지 영역 스타일 추가

--- src/page/InquiryPage.tsx
+++ src/page/InquiryPage.tsx
@@ -3,5 +3,12 @@
 import { jsx, css } from '@emotion/react';
 
 export default function InquiryPage() {
-	return <div>1대1 문의</div>;
+	return <div css={contentCss}>1대1 문의</div>;
 }
+
+const contentCss = css`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	min-height: 500px;
+`;
