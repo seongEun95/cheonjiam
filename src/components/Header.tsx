20d9ad0f6f68e27763ce93fdaa90bc0a68ac614b
seong eun <17adm@naver.com>
2023-12-20 00:32:33 +0900

    UserMenu컴포넌트 명을 Header컴포넌트명으로 변경

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,98 +2,26 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import menuData from '../data/menuData.json';
+import userMenuData from '../data/userMenuData.json';
 
-export default function Header() {
-	const [isShow, setIsShow] = useState<number | null>(null);
-
-	const handleMenuToggle = (index: number) => {
-		console.log(index);
-		setIsShow(prev => {
-			console.log('이전 값 : ' + prev);
-			return prev === index ? null : index;
-		});
-	};
-
+export default function UserMenu() {
 	return (
-		<header css={headerCss}>
-			<div css={logoCss}>
-				<h1>
-					<a href="/">
-						<img src={process.env.PUBLIC_URL + '/img/logo.png'} alt="천지암 로고" />
-					</a>
-				</h1>
-			</div>
-			<nav>
-				<ul>
-					{menuData.map((menu, index) => (
-						<li css={menuListCss} key={index}>
-							<span
-								onClick={() => {
-									console.log('클릭한 메뉴의 인덱스 번호 : ' + index);
-									handleMenuToggle(index);
-								}}
-								css={titleMenuCss}
-							>
-								{menu.label}
-							</span>
-
-							{isShow === index && (
-								<ul css={subMenuWrapCss}>
-									{menu.submenu.map((submenuItem, subIndex) => (
-										<li key={subIndex}>
-											<Link css={subMenuCss} to={submenuItem.url}>
-												{submenuItem.label}
-											</Link>
-										</li>
-									))}
-								</ul>
-							)}
-						</li>
-					))}
-				</ul>
-			</nav>
-		</header>
+		<ul css={userMenuWrapCss}>
+			{userMenuData.map((icon, index) => (
+				<li key={index}>
+					<Link to={icon.to}>
+						<img src={process.env.PUBLIC_URL + icon.imgSrc} alt={icon.alt} />
+					</Link>
+				</li>
+			))}
+		</ul>
 	);
 }
 
-const headerCss = css`
-	position: fixed;
-	top: 0;
-	left: 0;
-	width: 222px;
-	padding: 40px 25px;
-	box-sizing: border-box;
-`;
-
-const logoCss = css`
-	margin-bottom: 80px;
-	text-align: center;
-`;
-
-const menuListCss = css`
-	margin-bottom: 20px;
-`;
-
-const titleMenuCss = css`
-	display: inline-block;
-	font-size: 16px;
-	font-weight: 700;
-	color: #221e1f;
-	cursor: pointer;
-`;
-
-const subMenuWrapCss = css`
-	margin-top: 7px;
-`;
-
-const subMenuCss = css`
-	display: inline-block;
-	font-size: 14px;
-	font-weight: 500;
-	color: #656565;
-	letter-spacing: 0.02px;
-	margin-top: 5px;
-	margin-bottom: 5px;
+const userMenuWrapCss = css`
+	display: flex;
+	justify-content: flex-end;
+	align-items: center;
+	gap: 22px;
+	padding: 30px;
 `;
