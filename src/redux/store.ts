6dfd021a43d818602c0e9f640419297db4f724c8
seong eun <17adm@naver.com>
2023-12-20 00:31:12 +0900

    리덕스 툴킷 1.9.5 설치로 로거 정상 작동

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,15 +3,13 @@ import logger from 'redux-logger';
 import countSlice from './slice/countSlice';
 
 const middlewares = [];
-if (process.env.NODE_ENV === 'development') {
-	middlewares.push(logger);
-}
+if (process.env.NODE_ENV === 'development') middlewares.push(logger);
 
 const store = configureStore({
 	reducer: {
 		count: countSlice,
 	},
-	// middleware: middlewares,
+	middleware: middlewares,
 });
 
 export default store;
