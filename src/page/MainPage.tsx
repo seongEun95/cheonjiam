99862ae69a2978803a50cd6863eae5208f81b67b
seong eun <17adm@naver.com>
2023-12-27 08:49:46 +0900

    메인페이지 세로형 제품 정렬

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -4,11 +4,23 @@ import { jsx, css } from '@emotion/react';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../redux/store';
 import { plusCount } from '../redux/slice/countSlice';
-import ProductCard from '../components/ProductCardVertical';
+import ProductCardVertical from '../components/ProductCardVertical';
+import { useState } from 'react';
+import { PRODUCT_DATA } from '../data/productSample';
+import { useNavigate } from 'react-router-dom';
 
 export default function MainPage() {
 	const dispatch = useDispatch();
 	const count = useSelector((state: RootState) => state.count);
+
+	const [data] = useState(PRODUCT_DATA.filter((_, index) => index < 8)); // 인덱스 0 ~ 7까지만 생성, 총 8개 제품만 보여주기
+
+	const navigate = useNavigate();
+
+	const handleClickCard = (cardId: number) => {
+		console.log(cardId);
+		navigate(`/red_ginseng/${cardId}`);
+	};
 
 	return (
 		<div css={mainWrapCss}>
@@ -23,7 +35,11 @@ export default function MainPage() {
 					+
 				</button>
 			</div>
-			<div>{/* <ProductCard /> */}</div>
+			<div css={ProductCardVerticalWrapCss}>
+				{data.map(data => {
+					return <ProductCardVertical data={data} onClick={handleClickCard} />;
+				})}
+			</div>
 		</div>
 	);
 }
@@ -31,4 +47,9 @@ export default function MainPage() {
 const mainWrapCss = css`
 	min-height: 1000px;
 	background-color: #f9f9f9;
+`;
+
+const ProductCardVerticalWrapCss = css`
+	display: grid;
+	grid-template-columns: repeat(4, minmax(0, 275px));
 `;
