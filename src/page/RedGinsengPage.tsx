4d58778438109f7434cf7b71c08285664d2f3458
seong eun <17adm@naver.com>
2023-12-27 08:45:04 +0900

    제품 데이터 정렬

--- src/page/RedGinsengPage.tsx
+++ src/page/RedGinsengPage.tsx
@@ -1,8 +1,34 @@
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
-import { contentCss } from '../style/commonStyle';
+import { ProductHorizontalList } from '../types/ProductHorizontal.type';
+import { PRODUCT_HORIZONTAL_DATA } from '../data/productHorizontal';
+import ProductCardHorizontal from '../components/ProductCardHorizontal';
+import { useState } from 'react';
 
 export default function RedGinsengPage() {
-	return <div css={contentCss}>홍삼 페이지</div>;
+	const [data] = useState<ProductHorizontalList>(PRODUCT_HORIZONTAL_DATA.filter((_, index) => index < 8));
+
+	return (
+		<div>
+			<div>
+				<h2 css={titleCss}>홍삼</h2>
+			</div>
+			<div css={ProductCardWrapCss}>
+				{data.map(data => {
+					return <ProductCardHorizontal ProductData={data} />;
+				})}
+			</div>
+		</div>
+	);
 }
+
+const ProductCardWrapCss = css`
+	display: grid;
+	grid-template-columns: repeat(2, minmax(0, 535px));
+	gap: 40px;
+`;
+
+const titleCss = css`
+	text-align: center;
+`;
