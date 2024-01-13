01fe8781488c7a77bbbc13af5d44feccccc8fcad
seong eun <17adm@naver.com>
2024-01-14 01:09:04 +0900

    범위슬라이드 복습코드

--- src/page/RedGinsengPage.tsx
+++ src/page/RedGinsengPage.tsx
@@ -5,23 +5,19 @@ import { ProductHorizontalList } from '../types/ProductHorizontal.type';
 import { PRODUCT_HORIZONTAL_DATA } from '../data/productHorizontal';
 import ProductCardHorizontal from '../components/ProductCardHorizontal';
 import { useState } from 'react';
-import RangeSlider from '../components/ui/RangeSlider';
+import RangeSlider, { SelectedRange } from '../components/ui/RangeSlider';
+
+const min = 1000; // 더블슬라이드 최소값
+const max = 280000; // 더블슬라이드 최대값, 최소값과 최대값을 함수컴포넌트 밖으로 빼는 이유 : 컴포넌트 안에 변수가 있으면 컴포넌트가 계속 렌더링되면서 변수도 같이 메모리에 영향을 주므로 밖에서 선언한다.
 
 export default function RedGinsengPage() {
 	const [data] = useState<ProductHorizontalList>(PRODUCT_HORIZONTAL_DATA.filter((_, index) => index < 8));
 
-	const [minValue, setMinValue] = useState(275);
-	const [maxValue, setMaxValue] = useState(165000);
+	const [range, setRange] = useState([min, max]); // 슬라이드의 최소값, 최대값을 배열 상태값으로 설정
+	const handleChangeRange = (range: SelectedRange) => {
+		setRange([range[0].price, range[1].price]); // 최소밗, 최대값 가격을 상태변경
 
-	const fixMinValue = 275;
-	const fixMaxValue = 165000;
-
-	const handleDragSetMinValue = (minVal: number) => {
-		setMinValue(() => minVal);
-	};
-
-	const handleDragSetMaxValue = (maxVal: number) => {
-		setMaxValue(() => maxVal);
+		// Range값을 벡엔드 서버에 api 요청함.
 	};
 
 	return (
@@ -41,15 +37,7 @@ export default function RedGinsengPage() {
 					</div>
 				</div>
 				<div css={sliderWrapCss}>
-					<RangeSlider
-						label="가격"
-						minValue={minValue}
-						maxValue={maxValue}
-						fixMinValue={fixMinValue}
-						fixMaxValue={fixMaxValue}
-						onDragGetMinValue={handleDragSetMinValue}
-						onDragGetMaxValue={handleDragSetMaxValue}
-					/>
+					<RangeSlider label="가격" min={min} max={max} onChangeRange={handleChangeRange} />
 				</div>
 			</div>
 			<div css={ProductCardWrapCss}>
