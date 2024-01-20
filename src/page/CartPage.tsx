08c596f5b0d51ddc1c955ac8aa4f8ed591892c4b
seong eun <17adm@naver.com>
2024-01-20 23:24:50 +0900

    장바구니 페이지 수정

--- src/page/CartPage.tsx
+++ src/page/CartPage.tsx
@@ -2,82 +2,43 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
 import { useSelector } from 'react-redux';
-import ProductLabel from '../components/horizontalCardUi/ProductLabel';
-import { useState } from 'react';
-import { FaHeart, FaRegHeart } from 'react-icons/fa';
-import { IoClose } from 'react-icons/io5';
-import { GoGift } from 'react-icons/go';
-import { CiCircleMinus, CiCirclePlus } from 'react-icons/ci';
+
+import CartItem from '../components/ui/CartItem';
+import { RootState } from '../redux/store';
+import { Cart } from '../redux/slice/cartSlice';
+import Button from '../components/ui/Button';
+import { BsCartX } from 'react-icons/bs';
 
 export default function CartPage() {
-	const [isLike, setIsLike] = useState(false);
-	const cartItems = useSelector((state: any) => state.cart.items);
-
-	const [productCount, setProductCount] = useState(1);
-
-	const handleClickLike = (e: React.MouseEvent<HTMLDivElement>) => {
-		e.stopPropagation();
-		setIsLike(prev => !prev);
-	};
-
-	const handleClickMinus = () => {
-		setProductCount(prev => prev - 1);
-	};
-
-	const handleClickPlus = () => {
-		setProductCount(prev => prev + 1);
-	};
+	const cartItems = useSelector((state: RootState) => state.cart.items);
 
 	return (
 		<div>
 			<h2 css={titleCss}>장바구니</h2>
 			<div css={cartWrapCss}>
 				<div css={itemWrapCss}>
-					{cartItems.map((item: any, index: number) => (
-						<div css={itemCss} key={item.productId}>
+					{cartItems.length === 0 ? (
+						<div css={emptyItemWrapCss}>
 							<div>
-								<input css={checkboxCss} type="checkbox" />
-								<img css={imgCss} src={item.src} alt={item.name} />
-							</div>
-							<div>
-								<div css={firstLineCss}>
-									<ProductLabel label={item.iconClsf} />
-									<div css={iconWrapCss}>
-										<span css={iconCss} onClick={handleClickLike}>
-											{isLike ? <FaHeart fill="#d53147" size={20} /> : <FaRegHeart size={20} />}
-										</span>
-										<span css={iconCss}>
-											<IoClose size={28} />
-										</span>
-									</div>
+								<div>
+									<BsCartX size={40} color="#999" />
 								</div>
-								<div css={secondLineCss}>
-									<div css={titleWrapCss}>
-										<div>{item.brandName}</div>
-										<div css={itemTitleCss}>{item.name}</div>
-									</div>
-									<div css={priceWrapCss}>
-										<div css={priceCss}>{item.normalPrice * productCount}원</div>
-										<div css={plusMinusBtnWrapCss}>
-											<span onClick={handleClickMinus} css={plusMinustnCss}>
-												<CiCircleMinus size={26} />
-											</span>
-											<span css={productCountCss}>{productCount}</span>
-											<span onClick={handleClickPlus} css={plusMinustnCss}>
-												<CiCirclePlus size={26} />
-											</span>
-										</div>
-									</div>
+								<div css={emptyItemTxt}>
+									장바구니에 담긴 상품이 없습니다.
+									<br /> 다양한 상품을 확인하고 장바구니에 담아보세요!
 								</div>
-								<div css={btnWrapCss}>
-									<button css={giftBtnCss}>
-										<GoGift size={22} />
-									</button>
-									<button css={purchaseBtnCss}>바로구매</button>
+								<div>
+									<Button size="medium" kind="secondary">
+										로그인 하기
+									</Button>
 								</div>
 							</div>
 						</div>
-					))}
+					) : (
+						cartItems.map(({ id, isChecked, product, productCount }: Cart) => (
+							<CartItem key={id} id={id} isChecked={isChecked} product={product} productCount={productCount} />
+						))
+					)}
 				</div>
 				<div css={priceInfoWrapCss}>
 					<ul css={priceInfoItemListCss}>
@@ -91,6 +52,12 @@ export default function CartPage() {
 							<span css={InfoTitleCss}>상품금액</span>
 							<span>
 								<span>0</span>원
+							</span>
+						</li>
+						<li css={priceInfoItemCss}>
+							<span css={InfoTitleCss}>배송비</span>
+							<span>
+								<span>3,000</span>원
 							</span>
 						</li>
 					</ul>
@@ -109,7 +76,9 @@ export default function CartPage() {
 						</li>
 					</ul>
 					<div css={orderWrapCss}>
-						<button css={orderCss}>주문하기</button>
+						<Button disabled={cartItems.length === 0 && true} size="xlarge">
+							주문하기
+						</Button>
 					</div>
 				</div>
 			</div>
@@ -137,107 +106,18 @@ const itemWrapCss = css`
 	border-top: 1px solid #aaa;
 `;
 
-const itemCss = css`
-	display: flex;
-	gap: 20px;
-	padding-bottom: 14px;
-	border-bottom: 1px solid #e5e5e5;
-`;
-
-const checkboxCss = css`
-	vertical-align: top;
-`;
-
-const imgCss = css`
-	width: 120px;
-	height: 120px;
-`;
-
-const firstLineCss = css`
-	display: flex;
-	justify-content: space-between;
-	align-items: flex-start;
-`;
-
-const iconWrapCss = css`
+const emptyItemWrapCss = css`
+	width: 760px;
+	height: 300px;
 	display: flex;
 	align-items: center;
-	gap: 4px;
-`;
-
-const iconCss = css`
-	cursor: pointer;
-`;
-
-const secondLineCss = css`
-	display: flex;
-	justify-content: space-between;
-	margin: 10px 0 20px;
-`;
-
-const titleWrapCss = css`
-	width: 500px;
-`;
-
-const itemTitleCss = css`
-	font-size: 16px;
-	font-weight: 600;
-	margin-top: 10px;
-`;
-
-const priceCss = css`
-	font-size: 18px;
-	font-weight: 500;
-	margin-bottom: 16px;
-`;
-
-const plusMinusBtnWrapCss = css`
-	display: flex;
-	align-items: center;
-	gap: 14px;
-`;
-
-const plusMinustnCss = css`
-	cursor: pointer;
-`;
-
-const productCountCss = css`
-	width: 20px;
+	justify-content: center;
 	text-align: center;
 `;
 
-const priceWrapCss = css`
-	display: flex;
-	flex-direction: column;
-	align-items: flex-end;
-`;
-
-const btnWrapCss = css`
-	display: flex;
-	justify-content: flex-end;
-	gap: 10px;
-`;
-
-const giftBtnCss = css`
-	padding-top: 3px;
-	border: 1px solid #ccc;
-`;
-
-const purchaseBtnCss = css`
-	width: 120px;
-	height: 40px;
-	line-height: 40px;
-	text-align: center;
-	font-size: 14px;
-	color: #fff;
-	background-color: #201f1f;
-	border: 1px solid #201f1f;
-	transition: all 0.3s;
-
-	&:hover {
-		color: #201f1f;
-		background-color: #fff;
-	}
+const emptyItemTxt = css`
+	margin: 20px 0;
+	line-height: 1.5;
 `;
 
 const priceInfoWrapCss = css`
@@ -249,6 +129,11 @@ const priceInfoWrapCss = css`
 const priceInfoItemListCss = css`
 	display: flex;
 	flex-direction: column;
+
+	&:first-of-type {
+		margin-bottom: 20px;
+		border-bottom: 1px solid #e5e5e5;
+	}
 `;
 
 const priceInfoItemCss = css`
@@ -263,23 +148,7 @@ const InfoTitleCss = css`
 `;
 
 const orderWrapCss = css`
-	margin-top: 40px;
+	margin-top: 20px;
 	padding-bottom: 30px;
 	border-bottom: 1px solid #201f1f;
-`;
-
-const orderCss = css`
-	width: 240px;
-	height: 50px;
-	font-size: 16px;
-	line-height: 50px;
-	color: #fff;
-	background-color: #201f1f;
-	border: 1px solid #201f1f;
-	transition: all 0.3s;
-
-	&:hover {
-		color: #201f1f;
-		background-color: #fff;
-	}
 `;
