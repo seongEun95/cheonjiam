7b446d858b0840bf6f043ccec207cd646e7d76ec
seong eun <17adm@naver.com>
2024-01-23 22:04:34 +0900

    제품 숫자 컴포넌트 수정

--- src/components/ui/ProductCount.tsx
+++ src/components/ui/ProductCount.tsx
@@ -2,32 +2,21 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
 import { CiCircleMinus, CiCirclePlus } from 'react-icons/ci';
-import { minusProductCount, plusProductCount } from '../../redux/slice/cartSlice';
-import { useDispatch } from 'react-redux';
 
 type ProductCountProps = {
-	id?: number;
 	productCount?: number;
+	onClickLeft: React.MouseEventHandler<HTMLSpanElement>;
+	onClickRight: React.MouseEventHandler<HTMLSpanElement>;
 };
 
-export default function ProductCount({ id, productCount }: ProductCountProps) {
-	const dispatch = useDispatch();
-
-	const handleClickPlusProductCount = () => {
-		dispatch(plusProductCount(id));
-	};
-
-	const handleClickMinusProductCount = () => {
-		dispatch(minusProductCount(id));
-	};
-
+export default function ProductCount({ productCount, onClickLeft, onClickRight }: ProductCountProps) {
 	return (
 		<div css={plusMinusBtnWrapCss}>
-			<span onClick={handleClickMinusProductCount} css={plusMinusBtnCss}>
+			<span onClick={onClickLeft} css={plusMinusBtnCss}>
 				<CiCircleMinus size={26} />
 			</span>
 			<span css={productCountCss}>{productCount}</span>
-			<span onClick={handleClickPlusProductCount} css={plusMinusBtnCss}>
+			<span onClick={onClickRight} css={plusMinusBtnCss}>
 				<CiCirclePlus size={26} />
 			</span>
 		</div>
