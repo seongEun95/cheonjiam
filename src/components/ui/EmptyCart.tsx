44068d630a70b88daea00baa257d7b6af4a1ea84
seong eun <17adm@naver.com>
2024-01-23 22:05:23 +0900

    장바구니 페이지 비어있을 때 컴포넌트

---
+++ src/components/ui/EmptyCart.tsx
@@ -0,0 +1,40 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react';
+import { BsCartX } from 'react-icons/bs';
+import Button from './Button';
+
+export default function EmptyCart() {
+	return (
+		<div css={emptyItemWrapCss}>
+			<div>
+				<div>
+					<BsCartX size={40} color="#999" />
+				</div>
+				<div css={emptyItemTxt}>
+					장바구니에 담긴 상품이 없습니다.
+					<br /> 다양한 상품을 확인하고 장바구니에 담아보세요!
+				</div>
+				<div>
+					<Button size="medium" kind="secondary">
+						로그인 하기
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+const emptyItemWrapCss = css`
+	width: 760px;
+	height: 300px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	text-align: center;
+`;
+
+const emptyItemTxt = css`
+	margin: 20px 0;
+	line-height: 1.5;
+`;
