2f0fc4a0f0d32251a62fc97935418e759bd1c0a4
seong eun <17adm@naver.com>
2024-02-06 22:07:36 +0900

    홍삼페이지 min, max 수정

--- src/page/RedGinsengPage.tsx
+++ src/page/RedGinsengPage.tsx
@@ -2,23 +2,20 @@
 /** @jsx jsx */
 import { jsx, css } from '@emotion/react';
 import { ProductHorizontalList } from '../types/ProductHorizontal.type';
-import { PRODUCT_HORIZONTAL_DATA } from '../data/productHorizontal';
 import ProductCardHorizontal from '../components/ProductCardHorizontal';
 import { useEffect, useState } from 'react';
 import RangeSlider, { SelectedRange } from '../components/ui/RangeSlider';
 import axios from 'axios';
 import { refreshTokens } from '../api/refresh.api';
 
-const min = 1000; // 더블슬라이드 최소값
-const max = 280000; // 더블슬라이드 최대값, 최소값과 최대값을 함수컴포넌트 밖으로 빼는 이유 : 컴포넌트 안에 변수가 있으면 컴포넌트가 계속 렌더링되면서 변수도 같이 메모리에 영향을 주므로 밖에서 선언한다.
-
 export default function RedGinsengPage() {
-	// const [data] = useState<ProductHorizontalList>(PRODUCT_HORIZONTAL_DATA.filter((_, index) => index < 8));
+	const [range, setRange] = useState([1000, 280000]); // 슬라이드의 최소값, 최대값을 배열 상태값으로 설정
+	const [newData, setNewData] = useState<ProductHorizontalList>([]);
+	const [load, setLoad] = useState(false);
+	const [error, setError] = useState(false);
 
-	const [range, setRange] = useState([min, max]); // 슬라이드의 최소값, 최대값을 배열 상태값으로 설정
 	const handleChangeRange = (range: SelectedRange) => {
 		setRange([range[0].price, range[1].price]); // 최소밗, 최대값 가격을 상태변경
-		console.log(range);
 
 		setError(false);
 		setLoad(true);
@@ -34,14 +31,10 @@ export default function RedGinsengPage() {
 				setLoad(false);
 			})
 			.catch(err => {
-				console.log(err);
+				console.error(err);
 				setError(true);
 			});
 	};
-
-	const [newData, setNewData] = useState<ProductHorizontalList>([]);
-	const [load, setLoad] = useState(false);
-	const [error, setError] = useState(false);
 
 	useEffect(() => {
 		setError(false);
@@ -51,8 +44,8 @@ export default function RedGinsengPage() {
 		const getHongsamList = async () => {
 			const at = localStorage.getItem('at');
 
-			return await axios // @ts-ignore
-				.get(`http://localhost:8000/hongsam?min=${min}&max=${max}`, {
+			return await axios
+				.get(`http://localhost:8000/hongsam?min=${range[0]}&max=${range[1]}`, {
 					headers: { Authorization: `Bearer ${at}` },
 				})
 				.then(res => {
@@ -99,7 +92,7 @@ export default function RedGinsengPage() {
 					</div>
 				</div>
 				<div css={sliderWrapCss}>
-					<RangeSlider label="가격" min={min} max={max} onChangeRange={handleChangeRange} />
+					<RangeSlider label="가격" min={range[0]} max={range[1]} onChangeRange={handleChangeRange} />
 				</div>
 			</div>
 			<div css={ProductCardWrapCss}>
@@ -145,13 +138,16 @@ const ProductCardWrapCss = css`
 
 const loadingCss = css`
 	position: absolute;
+	left: 50%;
 	top: 50%;
-	padding: 10px;
+	transform: translate(-50%, -50%);
+	width: 200px;
+	height: 200px;
+	line-height: 200px;
 	color: #fff;
+	border-radius: 100%;
 	background-color: #d53147;
 	z-index: 10;
-	width: 1075px;
-	line-height: 80px;
 	text-align: center;
 	animation: opacity 0.4s infinite alternate;
 
